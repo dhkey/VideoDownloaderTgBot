c929ce3c3b4d7cb0b5d08791455fcf3890ba386f
dhkey <dhkey14@icloud.com>
2024-11-12 22:05:40 +0200

    project reformat

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import json 
 
+#importing routers
+from handlers.tiktok import router as tiktok_router
+
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token = json.load(open("config.json"))["token"] )
 dp = Dispatcher()
@@ -13,6 +16,9 @@ async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
 async def main():
+    #registering routers
+    dp.include_routers( tiktok_router )
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
